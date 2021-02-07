a2fa96247aa2b91fd2fbdf8e87793179ca5fa5d8
Daniel <daniel.grabowski1@gmail.com>
2021-02-07 11:00:09 -0300

    criar service facade para acesso ao estado

---
+++ src/app/services/pokemon-state-facade.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Select, Store } from '@ngxs/store';
+import { Observable } from 'rxjs';
+import { LoadPokemonGenerationsAction } from '../store/pokemon/pokemon.actions';
+import { PokemonGenerationResult } from '../store/pokemon/pokemon.model';
+import { PokemonState } from '../store/pokemon/pokemon.state';
+
+@Injectable({providedIn: 'root'})
+export class PokemonStateFacadeService {
+  @Select(PokemonState.generations) generations$: Observable<PokemonGenerationResult[]>;
+
+  constructor(private store: Store) { }
+
+  /**
+   * Loads the generations into the state
+   */
+  loadPokemonGenerations(): void {
+    this.store.dispatch(new LoadPokemonGenerationsAction());
+  }
+}
